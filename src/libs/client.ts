3d55218205ad41372b666937ee9caff0439836b2
ke-sato <keta0616@icloud.com>
2025-05-06 19:38:34 +0900

    環境変数を追加

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -1,10 +1,17 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-
-export const client = new DynamoDBClient({
-  region: "us-west-2",
-  endpoint: "http://dynamodb:8000", // ローカルDynamoDB
-  credentials: {
-    accessKeyId: "dummy",
-    secretAccessKey: "dummy",
-  },
-});
+import { config } from "dotenv";
+export async function DynamoClient() {
+  config();
+  console.log('envファイルの内様', process.env.DYNAMODB_ENDPOINT);
+  console.log('envファイルの内様', process.env.AWS_ACCESS_KEY_ID);
+  console.log('envファイルの内様', process.env.AWS_SECRET_ACCESS_KEY);
+  const client = new DynamoDBClient({
+    region: "ap-northeast-1",
+    endpoint: process.env.DYNAMODB_ENDPOINT || undefined, 
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
+  });
+  return client;
+}
